fc5038c9c4bfaeed7dc96c695fc0cdb7034f17a0
tsundori25 <tsundori25@gmail.comgit config --global user.email tsundori25@gmail.com>
2021-07-14 20:58:25 +0700

    first-deploy

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <div class="container">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+#app {
+  background-color: aqua;
+}
+</style>
